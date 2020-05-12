b9cbf25cc15cb3374a1e8085e0b5c6f9ffb285fe
rettchai <rettchai@gmail.com>
2020-05-12 10:36:20 +0700

    ส่งการบ้าน

---
+++ loop.dart
@@ -0,0 +1,7 @@
+void main() {
+ var list = const [1, 2, 3];
+  for (var n in list) {
+  print(n);
+}
+
+}
---
+++ square_function.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(squareC(10));
+  print(squareD(15,5));
+}
+
+int squareC(int n) {
+  return n * n;
+}
+int squareD(int n,int m) {
+  return n * m;
+}
